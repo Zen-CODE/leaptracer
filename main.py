92613fb3fda171c65d681d34f3655dc41d66e5e7
Zen-CODE <zenkey.zencode@gmail.com>
2020-09-15 19:08:00 +0200

    :zap: Added draw_motion as an option

--- main.py
+++ main.py
@@ -30,6 +30,8 @@ from kivy.graphics import Color, Rectangle, Point, GraphicException, Line
 from random import random
 from math import sqrt
 from kivy.core.window import Window
+from time import time
+from kivy.properties import BooleanProperty
 
 
 def calculate_points(x1, y1, x2, y2, steps=5):
@@ -49,6 +51,11 @@ def calculate_points(x1, y1, x2, y2, steps=5):
 
 
 class Leaptracer(FloatLayout):
+    """
+    The Main layout file. For styling and contents, please see the leaptracer.kv
+    file.
+    """
+    draw_motion = BooleanProperty(True)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -80,84 +87,82 @@ class Leaptracer(FloatLayout):
                 pos[0], pos[1] + cross_width]
 
     def on_touch_down(self, touch):
-        win = self.get_parent_window()
-        ud = touch.ud
-        ud['group'] = g = str(touch.uid)
-        pointsize = 5
-        if 'pressure' in touch.profile:
-            ud['pressure'] = touch.pressure
-            pointsize = (touch.pressure * 100000) ** 2
-        ud['color'] = random()
+        if self.draw_motion:
+            win = self.get_parent_window()
+            ud = touch.ud
+            ud['group'] = g = str(touch.uid)
+            pointsize = 5
+            if 'pressure' in touch.profile:
+                ud['pressure'] = touch.pressure
+                pointsize = (touch.pressure * 100000) ** 2
+            ud['color'] = random()
 
-        with self.canvas:
-            Color(ud['color'], 1, 1, mode='hsv', group=g)
-            ud['lines'] = [
-                Rectangle(pos=(touch.x, 0), size=(1, win.height), group=g),
-                Rectangle(pos=(0, touch.y), size=(win.width, 1), group=g),
-                Point(points=(touch.x, touch.y), source='particle.png',
-                      pointsize=pointsize, group=g)]
+            with self.canvas:
+                Color(ud['color'], 1, 1, mode='hsv', group=g)
+                ud['lines'] = [
+                    Rectangle(pos=(touch.x, 0), size=(1, win.height), group=g),
+                    Rectangle(pos=(0, touch.y), size=(win.width, 1), group=g),
+                    Point(points=(touch.x, touch.y), source='particle.png',
+                        pointsize=pointsize, group=g)]
 
-        ud['label'] = Label(size_hint=(None, None))
-        self.update_touch_label(ud['label'], touch)
-        self.add_widget(ud['label'])
-        touch.grab(self)
+            ud['label'] = Label(size_hint=(None, None))
+            self.update_touch_label(ud['label'], touch)
+            self.add_widget(ud['label'])
+            touch.grab(self)
         return super().on_touch_down(touch)
 
     def on_touch_move(self, touch):
-        if touch.grab_current is not self:
-            return
-        ud = touch.ud
-        ud['lines'][0].pos = touch.x, 0
-        ud['lines'][1].pos = 0, touch.y
+        if touch.grab_current is self or self.draw_motion:
+            ud = touch.ud
+            ud['lines'][0].pos = touch.x, 0
+            ud['lines'][1].pos = 0, touch.y
 
-        index = -1
+            index = -1
 
-        while True:
-            try:
-                points = ud['lines'][index].points
-                oldx, oldy = points[-2], points[-1]
-                break
-            except Exception:
-                index -= 1
+            while True:
+                try:
+                    points = ud['lines'][index].points
+                    oldx, oldy = points[-2], points[-1]
+                    break
+                except Exception:
+                    index -= 1
 
-        points = calculate_points(oldx, oldy, touch.x, touch.y)
+            points = calculate_points(oldx, oldy, touch.x, touch.y)
 
-        # if pressure changed create a new point instruction
-        if 'pressure' in ud:
-            if not .95 < (touch.pressure / ud['pressure']) < 1.05:
-                g = ud['group']
-                pointsize = (touch.pressure * 100000) ** 2
-                with self.canvas:
-                    Color(ud['color'], 1, 1, mode='hsv', group=g)
-                    ud['lines'].append(
-                        Point(points=(), source='particle.png',
-                              pointsize=pointsize, group=g))
+            # if pressure changed create a new point instruction
+            if 'pressure' in ud:
+                if not .95 < (touch.pressure / ud['pressure']) < 1.05:
+                    g = ud['group']
+                    pointsize = (touch.pressure * 100000) ** 2
+                    with self.canvas:
+                        Color(ud['color'], 1, 1, mode='hsv', group=g)
+                        ud['lines'].append(
+                            Point(points=(), source='particle.png',
+                                pointsize=pointsize, group=g))
 
-        if points:
-            try:
-                lp = ud['lines'][-1].add_point
-                for idx in range(0, len(points), 2):
-                    lp(points[idx], points[idx + 1])
-            except GraphicException:
-                pass
+            if points:
+                try:
+                    lp = ud['lines'][-1].add_point
+                    for idx in range(0, len(points), 2):
+                        lp(points[idx], points[idx + 1])
+                except GraphicException:
+                    pass
 
-        ud['label'].pos = touch.pos
-        import time
-        t = int(time.time())
-        if t not in ud:
-            ud[t] = 1
-        else:
-            ud[t] += 1
-        self.update_touch_label(ud['label'], touch)
+            ud['label'].pos = touch.pos
+            t = int(time())
+            if t not in ud:
+                ud[t] = 1
+            else:
+                ud[t] += 1
+            self.update_touch_label(ud['label'], touch)
         return super().on_touch_move(touch)
 
     def on_touch_up(self, touch):
-        if touch.grab_current is not self:
-            return
-        touch.ungrab(self)
-        ud = touch.ud
-        self.canvas.remove_group(ud['group'])
-        self.remove_widget(ud['label'])
+        if touch.grab_current:
+            touch.ungrab(self)
+            ud = touch.ud
+            self.canvas.remove_group(ud['group'])
+            self.remove_widget(ud['label'])
         return super().on_touch_up(touch)
 
     def update_touch_label(self, label, touch):
@@ -173,7 +178,6 @@ class LeaptracerApp(App):
     icon = 'icon.png'
 
     def build(self):
-        # Builder.load_file("leaptracer.kv")
         return Leaptracer()
 
     def on_pause(self):
